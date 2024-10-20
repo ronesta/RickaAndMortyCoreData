39ab6273de0f1869c3664e14683741d3d64efe75
ronesta <gvozzzd123@gmail.com>
2024-10-20 18:52:06 +0300

    Initially Setting Up a ViewController

--- RickaAndMortyCoreData/RickaAndMortyCoreData/ViewController.swift
+++ RickaAndMortyCoreData/RickaAndMortyCoreData/ViewController.swift
@@ -6,11 +6,58 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationBar()
+        setupViews()
+    }
+
+    private func setupNavigationBar() {
+        title = "Characters"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.backgroundColor = .white
+    }
+
+    private func setupViews() {
         view.backgroundColor = .white
+        view.addSubview(tableView)
+
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalToSuperview()
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        7
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        150
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
