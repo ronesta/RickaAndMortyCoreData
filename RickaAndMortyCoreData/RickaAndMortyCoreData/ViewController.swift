6c2f6b4338a81f9a2198bf236692ae7819de028b
ronesta <gvozzzd123@gmail.com>
2024-10-20 18:23:59 +0300

    Initial commit

---
+++ RickaAndMortyCoreData/RickaAndMortyCoreData/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  RickaAndMortyCoreData
+//
+//  Created by Ибрагим Габибли on 20.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
